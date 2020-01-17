75acd7a1fd24ffdb5c85ceaf23955075cd1d3d32
Karel Muniz <karel.muaizperdomo@nike.com>
2020-01-17 13:50:41 +0100

    First project

---
+++ cashtrack/src/main/java/net/kmuniz/cashtrack/CashtrackApplication.java
@@ -0,0 +1,13 @@
+package net.kmuniz.cashtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashtrackApplication.class, args);
+	}
+
+}
---
+++ cashtrack/src/test/java/net/kmuniz/cashtrack/CashtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kmuniz.cashtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
